4ab80e29a3aa5c9d063ac2328bef3d0956b161e6
Svetttllana <Szgirovskaya@mail.ru>
2022-11-25 23:12:40 +0300

    less20; RecyclerView; Fragment;

--- app/src/main/java/com/example/clswrk_androidprojekt/MainActivity.kt
+++ app/src/main/java/com/example/clswrk_androidprojekt/MainActivity.kt
@@ -9,40 +9,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val btnGoToActivity2 = findViewById<Button>(R.id.btnGoToActivity2)
-        btnGoToActivity2.setOnClickListener {
-            startActivity(Intent(this,MainActivity2::class.java)
-                .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
-            //MainActivity2  не будет добавлена в бекстек
-                //.addFlags(Intent.FLAG_Activity_NO_HISTORY
-        }
+
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.activity_container,OnBoardingFragment())
+        fragmentTransaction.commit()
+
     }
 }
 
 
-//    override fun onStart() {
-//        super.onStart()
-//        Toast.makeText(this, "OnStart", Toast.LENGTH_SHORT).show()
-//    }
-//
-//    override fun onResume() {
-//        super.onResume()
-//        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show()
-//    }
-//
-//    override fun onPause() {
-//        super.onPause()
-//        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show()
-//    }
-//
-//    override fun onStop() {
-//        super.onStop()
-//        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show()
-//    }
-//
-//    override fun onDestroy() {
-//        super.onDestroy()
-//        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show()
-//    }
-//}
