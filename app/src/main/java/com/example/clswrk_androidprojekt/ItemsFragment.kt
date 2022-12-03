04e64db899a58c4c73855a5b0b06df5f5ac0547c
Svetttllana <Szgirovskaya@mail.ru>
2022-12-03 16:01:41 +0300

    less22; disignPatterns

--- app/src/main/java/com/example/clswrk_androidprojekt/ItemsFragment.kt
+++ app/src/main/java/com/example/clswrk_androidprojekt/ItemsFragment.kt
@@ -11,15 +11,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-
+import com.example.clswrk_androidprojekt.ItemsViewModel
 
 
 class ItemsFragment : Fragment(), ItemsListener {
 
 
     private lateinit var itemsAdapter: ItemsAdapter
+    private val viewModel: ItemsViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,48 +41,42 @@ class ItemsFragment : Fragment(), ItemsListener {
         recyclerView.layoutManager = LinearLayoutManager(context)
         recyclerView.adapter = itemsAdapter
 
+        viewModel.getData()
+        viewModel.items.observe(viewLifecycleOwner) { listItems ->
+            itemsAdapter.submitList(listItems)
+        }
+        viewModel.msg.observe(viewLifecycleOwner) { msg ->
+            Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+        }
+        viewModel.bundle.observe(viewLifecycleOwner) { navBundle ->
 
-        val listItems = listOf<ItemsModel>(
-            ItemsModel(R.drawable.banana, "Android", "20.20.2020"),
-            ItemsModel(R.drawable.apple, " IOS,", "10.10.2020"),
-            ItemsModel(R.drawable.rofl_kit3, "Flutter", "19.20.2022"),
-            ItemsModel(R.drawable.rofl, "Python", "20.10.2023"),
-            ItemsModel(R.drawable.apple, "Xamarin", "02.04.2024"),
-            ItemsModel(R.drawable.banana, ".NET", "20.20.2020"),
-            ItemsModel(R.drawable.rose_image, "C++", "06.05.2020"),
-            ItemsModel(R.drawable.rofl_kit3, "C", "07.10.2020"),
-            ItemsModel(R.drawable.rofl_kit2, "PHP", "20.06.2020"),
-            ItemsModel(R.drawable.chmonya_image, "Ruby on Rails", "20.06.2020"),
-            ItemsModel(R.drawable.rofl, "JS", "20.06.2020"),
-            ItemsModel(R.drawable.mem_kit_ofise, "apple", "20.06.2020"),
-            ItemsModel(R.drawable.mem_kit_image, "pineaple", "20.06.2020")
+            val detailsFragment = DetailsFragment()
+            // в бвндле храми маленькие данные и плюс можно там прописать ключик и имя ключика,
+            // которые мы сможем передать в аргументы
+            val bundle = Bundle()
+            bundle.putString("name", navBundle.name)
+            bundle.putString("date", navBundle.date)
+            bundle.putInt("imageView", navBundle.image)
 
-        )
-        itemsAdapter.submitList(listItems.toList())
+            detailsFragment.arguments = bundle
 
+
+            parentFragmentManager
+                .beginTransaction()
+                .replace(R.id.activity_container, detailsFragment)
+                .commit()
+
+        }
 
     }
 
+
     override fun onClick() {
-        Toast.makeText(context, "ImageView clicked" , Toast.LENGTH_SHORT).show()
+        viewModel.imageViewClicked()
     }
 
     override fun onElementSelected(name: String, date: String, imageView: Int) {
-        val detailsFragment = DetailsFragment()
-        // в бвндле храми маленькие данные и плюс можно там прописать ключик и имя ключика,
-        // которые мы сможем передать в аргументы
-        val bundle = Bundle()
-        bundle.putString("name",name)
-        bundle.putString("date",date)
-        bundle.putInt("imageView",imageView)
-
-        detailsFragment.arguments = bundle
-
-
-        parentFragmentManager
-            .beginTransaction()
-            .add(R.id.activity_container,detailsFragment)
-                       .commit()
+        viewModel.elementClicked(name, date, imageView)
     }
 }
 
